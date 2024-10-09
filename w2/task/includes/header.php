e35ebe3bae3c01bab7cdfcebc99c9ae21de30ac3
Pandie05 <hreyes@email.neit.edu>
2024-10-08 22:27:18 -0400

    dooks

---
+++ w2/task/includes/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Hendry's Site</title>
+    </head>
+    <body>
---
+++ w2/task/index.php
@@ -0,0 +1,44 @@
+<?php include 'includes/header.php'; ?>
+
+<h1>Hendry Reyes</h1>
+
+<a href="https://github.com/Pandie05/hreyes-PHP" target="_blank">Github</a>
+
+<p>Helpful PHP Links:</p>
+<ul>
+    <li><a href="https://www.php.net" target="_blank">Link 1</a></li>
+    <li><a href="https://www.php.net/manual/en/control-structures.alternative-syntax.php" target="_blank">Link 2</a></li>
+    <li><a href="https://phptherightway.com" target="_blank">Link 3</a></li>
+</ul>
+
+<p>Helpful Git Links:</p>
+<ul>
+    <li><a href="https://git-scm.com/book/en/v2" target="_blank">link 1</a></li>
+    <li><a href="https://docs.github.com/en/get-started/getting-started-with-git/set-up-git" target="_blank">Link 2</a></li>
+    <li><a href="https://www.youtube.com/watch?v=0PFMg78bhYY" target="_blank">Link 3</a></li>
+</ul>
+
+<p>Things I Enjoy</p>
+<ul>
+    <li><a href="https://playvalorant.com/en-us/" target="_blank">Valorant</a></li>
+    <li><a href="https://www.youtube.com/watch?v=T6uk9XNqoMo" target="_blank">Piano</a></li>
+    <li><a href="https://www.uniqlo.com/us/en/men" target="_blank">Shopping</a></li>
+</ul>
+
+<p>Latest Update: <?php echo date("F d Y H:i:s.", filemtime(__FILE__)); ?></p>
+
+<p>Labs:</p>
+<ul>
+    <li><a href="../../w1/w1-task" target="_blank">Lab 1</a></li>
+    <li><a href="" target="_blank">Lab 2</a></li>
+    <li><a href="" target="_blank">Lab 3</a></li>
+    <li><a href="" target="_blank">Lab 4</a></li>
+    <li><a href="" target="_blank">Lab 5</a></li>
+    <li><a href="" target="_blank">Lab 6</a></li>
+    <li><a href="" target="_blank">Lab 7</a></li>
+    <li><a href="" target="_blank">Lab 8</a></li>
+    <li><a href="" target="_blank">Lab 9</a></li>
+    <li><a href="" target="_blank">Lab 10</a></li>
+</ul>
+
+<?php include 'includes/footer.php'; ?>
